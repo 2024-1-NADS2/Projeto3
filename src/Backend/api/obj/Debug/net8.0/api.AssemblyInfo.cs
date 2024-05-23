aa04db11a7e25ca17848ed231274e56483e2546f
deGuicS <guisilvaeco@gmail.com>
2024-05-23 16:03:03 -0300

    ajustes no vite config

--- src/Backend/api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ src/Backend/api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91c3c90d0b545464538f6719530e40bb1855d88c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fba70443bb3d7875b7bcecd957b846213848155f")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
